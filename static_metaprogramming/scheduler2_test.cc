8d8723331151cbf0c4ea3acbaf93a0b927ff2d46
mateus <mateus@kenobi.none>
2012-05-25 14:42:26 -0300

    Static metaprogramming examples.

---
+++ static_metaprogramming/scheduler2_test.cc
@@ -0,0 +1,12 @@
+// EPOS Scheduler Abstraction Test Program 2
+// This is just a point (C++ module) to include the EPOS's Scheduler header.
+
+
+#include <meta.h>
+
+using namespace System;
+
+int main()
+{
+    return 0;
+}
