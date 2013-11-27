58a418d16a9e5ac62e3815ce0ca4f259fc56b344
Mateus Krepsky Ludwich <mkludwich@gmail.com>
2013-11-27 10:25:23 -0200

    Bounds check counter example 01.

---
+++ cbmc_bounds_check_ce01/main.cc
@@ -0,0 +1,19 @@
+int main() 
+{
+    int dec[10];
+
+    for(int i = 0; i < 10; i++) 
+    {
+        dec[i] = i;
+    }
+    
+    dec[10] = 55;
+
+    for(int i = 0; i < 10; i++) 
+    {
+        assert(dec[i] == i);
+    }
+
+    return 0;
+}
+
