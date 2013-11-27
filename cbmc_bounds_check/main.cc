b601772142fb911e715aee243786d70acafef13a
Mateus Krepsky Ludwich <mkludwich@gmail.com>
2013-11-27 10:13:52 -0200

    indentation

--- cbmc_bounds_check/main.cc
+++ cbmc_bounds_check/main.cc
@@ -2,14 +2,17 @@ int main()
 {
     int dec[10];
 
-    for(int i = 0; i < 10; i++) {
-	dec[i] = i;
+    for(int i = 0; i < 10; i++) 
+    {
+        dec[i] = i;
     }
 
 
-    for(int i = 0; i < 10; i++) {
-	assert(dec[i] == i);
+    for(int i = 0; i < 10; i++) 
+    {
+        assert(dec[i] == i);
     }
 
     return 0;
 }
+
